7b6110a32576557c8dfe9c9a96e8ecadd196942e
Iroegbu David <iroegbu.dgit@gmail.com>
2024-12-02 17:06:01 +0100

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import {ChatProvider} from '@/app/context/ChatContext'
 export const metadata: Metadata = {
   title: "Cloud Bird",
   description: "An AI-driven platform that empowers entrepreneurs and businesses with tools for ideation, growth, funding, and real-time insights in a centralized ecosystem.",
-  keywords: "AI Business Tools, Entrepreneurship Platform, Business Growth Assistant, Funding and Planning, Market Insights",
+  keywords: "AI Business Tools, Entrepreneurship Platform, Business Growth Assistant, Funding and Planning, Market Insights, .",
 
 };
 
